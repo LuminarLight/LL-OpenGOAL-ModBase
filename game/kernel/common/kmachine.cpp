c6aa502dabac4d7410c6433b8aaa32fe89b3c596
Luminar Light <18116946+LuminarLight@users.noreply.github.com>
2024-07-18 17:05:55 +0200

    Fix game crash due to miniaudio if Reboot in Debug Mode

--- game/kernel/common/kmachine.cpp
+++ game/kernel/common/kmachine.cpp
@@ -64,7 +64,7 @@ void kmachine_init_globals_common() {
   vif1_interrupt_handler = 0;
   vblank_interrupt_handler = 0;
   ee_clock_timer = Timer();
-
+  ma_engine_uninit(&maEngine);
   ma_engine_init(NULL, &maEngine);
 }
 
